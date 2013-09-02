17d27ac72a27c0efa5eba89e5d230208087c1e18
Mihail Chilyashev <chilyashev@gmail.com>
2013-09-02 08:40:12 +0300

    Started messing around with some stuff

--- index.php
+++ index.php
@@ -5,6 +5,23 @@
     <title></title>
     <script src="js/core.js"></script>
     <script src="js/main.js"></script>
+    <style type="text/css">
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        html, body {
+            width:  100%;
+            height: 100%;
+            margin: 0;
+         }
+        canvas{
+            outline:0;
+            border:1px solid #000;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
 </head>
 <body id="body">
 </body>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-var Player = Class.extend({
+var Entity = Class.extend({
+    init: function(){
+        log('yes!');
+    }
+});
+var Player = Entity.extend({
     name: "Playa",
     x: 0,
     y: 0,
     max_hp: 0,
     hp: 0,
+    update: function(){
+        log("called")
+    },
     init: function(stuff){
         this.stuff = stuff;
         log(stuff);
@@ -14,16 +22,24 @@ var game = {
     canvas: null,
     context: null,
     body: null,
+    entities: [],
     g1: 0,
     init: function () {
         this.body = document.body;
         this.canvas = document.createElement('canvas');
-        this.canvas.width = document.width;
-        this.canvas.height = document.height;
+        this.canvas.width = w = document.width;
+        this.canvas.height = h = document.height;
 
         this.body.appendChild(this.canvas);
 
+        log(window.innerWidth);
+        log(document.width)
         var ctx = this.context = this.canvas.getContext('2d');
+        ctx.canvas.width  = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+        ctx.fillStyle = "#6666FF";
+        ctx.fillRect(0, 0, w, h);
+        ctx.fillStyle = "#000000";
         ctx.font="30px Arial";
         var str = "And thus some stuff shall happen.";
         ctx.fillText(str, document.width/2-(str.length*8), 50);
@@ -32,8 +48,15 @@ var game = {
     loadPlayer: function () {
         var playerStuff = getAsset('entities', 'player');
         this.player = new Player(playerStuff);
+        this.entities.push(this.player)
+    },
+    update: function(){
+        for(var entity in this.entities){
+            this.entities[entity].update();
+        }
     },
     render: function(){
+        this.update();
         var ctx = this.context;
         ctx.fillStyle = "rgb(200,"+this.g1+",0)";
         this.g1 = (this.g1+10) %  255;
@@ -46,4 +69,3 @@ window.onload = function () {
     game.init();
     setInterval(function(){game.render()}, 30);
 };
-
